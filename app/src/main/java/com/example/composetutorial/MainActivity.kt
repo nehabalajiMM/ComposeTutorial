ef5b683954d96acc17155493a86115df5cd25b76
nehabalaji <neha.balaji@mutualmobile.com>
2022-10-02 06:33:00 -0700

    animate messages while expanding

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -4,8 +4,11 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -13,7 +16,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -26,9 +29,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTutorialTheme{
-                Surface {
-                    MessageCard(Message("Android", "Jetpack Compose"))
-                }
+                Conversation(SampleData.conversationSample)
             }
         }
     }
@@ -51,7 +52,13 @@ class MainActivity : ComponentActivity() {
             )
             // Add a horizontal space between the image and the column
             Spacer(modifier = Modifier.width(8.dp))
-            Column {
+
+            var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor by animateColorAsState(
+                if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+            )
+
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
                 Text(
                     text = msg.author,
                     color = MaterialTheme.colors.secondaryVariant,
@@ -59,9 +66,14 @@ class MainActivity : ComponentActivity() {
                 )
                 // Add a vertical space between the author and message texts
                 Spacer(modifier = Modifier.height(4.dp))
-                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Surface(shape = MaterialTheme.shapes.medium,
+                    elevation = 1.dp,
+                    color = surfaceColor,
+                    modifier = Modifier.animateContentSize().padding(1.dp)) {
                     Text(
                         text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                         style = MaterialTheme.typography.body2
                     )
                 }
@@ -79,12 +91,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun PreviewMessageCard() {
         ComposeTutorialTheme {
-            Surface {
-                MessageCard(
-                    msg = Message("Colleague", "Hey, take a look at Jetpack Compose, it's great!")
-                )
-            }
-
+            Conversation(SampleData.conversationSample)
         }
     }
 
